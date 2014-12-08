646bfe4c5568463277794cfe075a99099aebc948
Philipp Otto <philipp@scm.io>
2014-12-08 22:21:42 +0100

    convert tabs to spaces

--- MultiEditUtils.py
+++ MultiEditUtils.py
@@ -4,369 +4,369 @@ import re
 
 class MultiFindAllCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit):
+  def run(self, edit):
 
-		view = self.view
+    view = self.view
 
-		for region in view.sel():
-			substr = view.substr(region)
-			for region in view.find_all(substr, sublime.LITERAL):
-				view.sel().add(region)
+    for region in view.sel():
+      substr = view.substr(region)
+      for region in view.find_all(substr, sublime.LITERAL):
+        view.sel().add(region)
 
 
 
 class JumpToLastRegionCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit):
+  def run(self, edit):
 
-		selection = self.view.sel()
-		lastRegion = selection[-1]
-		cursorPosition = lastRegion.b
-		selection.clear()
-		selection.add(sublime.Region(cursorPosition))
-		self.view.show(cursorPosition, False)
+    selection = self.view.sel()
+    lastRegion = selection[-1]
+    cursorPosition = lastRegion.b
+    selection.clear()
+    selection.add(sublime.Region(cursorPosition))
+    self.view.show(cursorPosition, False)
 
 
 class AddLastSelectionCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit):
+  def run(self, edit):
 
-		helper = Helper.getOrConstructHelperForView(self.view)
-		lastSelections = helper.lastSelections
+    helper = Helper.getOrConstructHelperForView(self.view)
+    lastSelections = helper.lastSelections
 
-		if len(lastSelections) < 1:
-			return
+    if len(lastSelections) < 1:
+      return
 
-		currentSelection = self.view.sel()
-		oldSelectionHash = Helper.hashSelection(currentSelection)
+    currentSelection = self.view.sel()
+    oldSelectionHash = Helper.hashSelection(currentSelection)
 
-		for region in lastSelections[-1]:
-			helper.ignoreSelectionCommand = True
-			currentSelection.add(region)
+    for region in lastSelections[-1]:
+      helper.ignoreSelectionCommand = True
+      currentSelection.add(region)
 
-		newSelectionHash = Helper.hashSelection(currentSelection)
+    newSelectionHash = Helper.hashSelection(currentSelection)
 
-		lastSelections.pop(-1)
+    lastSelections.pop(-1)
 
-		nothingChanged = oldSelectionHash == newSelectionHash
-		if nothingChanged:
-			# Rerun if the previous selection was only a subset of the current selection.
-			self.run(edit)
+    nothingChanged = oldSelectionHash == newSelectionHash
+    if nothingChanged:
+      # Rerun if the previous selection was only a subset of the current selection.
+      self.run(edit)
 
 
 class CycleThroughRegionsCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit):
+  def run(self, edit):
 
-		view = self.view
-		visibleRegion = view.visible_region()
-		selectedRegions = view.sel()
+    view = self.view
+    visibleRegion = view.visible_region()
+    selectedRegions = view.sel()
 
-		if not len(selectedRegions):
-			return
+    if not len(selectedRegions):
+      return
 
-		nextRegion = None
+    nextRegion = None
 
-		# Find the first region which comes after the visible region.
-		for region in selectedRegions:
-			if region.end() > visibleRegion.b:
-				nextRegion = region
-				break
+    # Find the first region which comes after the visible region.
+    for region in selectedRegions:
+      if region.end() > visibleRegion.b:
+        nextRegion = region
+        break
 
-		# If the last region in the buffer was reached, take the first region.
-		# Empty regions will be evaluated falsy, which is why short-circuit evaluation doesn't work here.
-		if nextRegion is None:
-			nextRegion = selectedRegions[0]
+    # If the last region in the buffer was reached, take the first region.
+    # Empty regions will be evaluated falsy, which is why short-circuit evaluation doesn't work here.
+    if nextRegion is None:
+      nextRegion = selectedRegions[0]
 
-		view.show(nextRegion, False)
+    view.show(nextRegion, False)
 
 
 
 class NormalizeRegionEndsCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit):
+  def run(self, edit):
 
-		view = self.view
-		selection = view.sel()
+    view = self.view
+    selection = view.sel()
 
-		if not len(selection):
-			return
+    if not len(selection):
+      return
 
-		if self.areRegionsNormalized(selection):
-			regions = self.invertRegions(selection)
-		else:
-			regions = self.normalizeRegions(selection)
+    if self.areRegionsNormalized(selection):
+      regions = self.invertRegions(selection)
+    else:
+      regions = self.normalizeRegions(selection)
 
-		selection.clear()
-		for region in regions:
-			selection.add(region)
+    selection.clear()
+    for region in regions:
+      selection.add(region)
 
-		firstVisibleRegion = self.findFirstVisibleRegion()
-		if firstVisibleRegion is not None:
-			# if firstVisibleRegion won't work with empty regions
-			view.show(firstVisibleRegion.b, False)
-
-
-	def findFirstVisibleRegion(self):
-
-		visibleRegion = self.view.visible_region()
-
-		for region in self.view.sel():
-			if region.intersects(visibleRegion):
-				return region
-
-		return None
+    firstVisibleRegion = self.findFirstVisibleRegion()
+    if firstVisibleRegion is not None:
+      # if firstVisibleRegion won't work with empty regions
+      view.show(firstVisibleRegion.b, False)
 
 
-	def normalizeRegions(self, regions):
+  def findFirstVisibleRegion(self):
 
-		return self.invertRegions(regions, lambda region: region.a > region.b)
+    visibleRegion = self.view.visible_region()
 
+    for region in self.view.sel():
+      if region.intersects(visibleRegion):
+        return region
 
-	def invertRegions(self, regions, condition = lambda region: True):
-
-		invertedRegions = []
-
-		for region in regions:
-			invertedRegion = region
-			if condition(region):
-				invertedRegion = sublime.Region(region.b, region.a)
-
-			invertedRegions.append(invertedRegion)
-
-		return invertedRegions
+    return None
 
 
-	def areRegionsNormalized(self, regions):
+  def normalizeRegions(self, regions):
 
-		return all(region.a < region.b for region in regions)
+    return self.invertRegions(regions, lambda region: region.a > region.b)
+
+
+  def invertRegions(self, regions, condition = lambda region: True):
+
+    invertedRegions = []
+
+    for region in regions:
+      invertedRegion = region
+      if condition(region):
+        invertedRegion = sublime.Region(region.b, region.a)
+
+      invertedRegions.append(invertedRegion)
+
+    return invertedRegions
+
+
+  def areRegionsNormalized(self, regions):
+
+    return all(region.a < region.b for region in regions)
 
 
 
 class SplitSelectionCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit, separator = None):
+  def run(self, edit, separator = None):
 
-		self.savedSelection = [r for r in self.view.sel()]
+    self.savedSelection = [r for r in self.view.sel()]
 
-		selectionSize = sum(map(lambda region: region.size(), self.savedSelection))
-		if selectionSize == 0:
-			# nothing to do
-			sublime.status_message("Cannot split an empty selection.")
-			return
+    selectionSize = sum(map(lambda region: region.size(), self.savedSelection))
+    if selectionSize == 0:
+      # nothing to do
+      sublime.status_message("Cannot split an empty selection.")
+      return
 
-		if separator != None:
-			self.splitSelection(separator)
-		else:
-			onConfirm, onChange = self.getHandlers()
+    if separator != None:
+      self.splitSelection(separator)
+    else:
+      onConfirm, onChange = self.getHandlers()
 
-			inputView = sublime.active_window().show_input_panel(
-				"Separating character(s) for splitting the selection",
-				" ",
-				onConfirm,
-				onChange,
-				self.restoreSelection
-			)
+      inputView = sublime.active_window().show_input_panel(
+        "Separating character(s) for splitting the selection",
+        " ",
+        onConfirm,
+        onChange,
+        self.restoreSelection
+      )
 
-			inputView.run_command("select_all")
-
-
-	def getHandlers(self):
-
-		settings = sublime.load_settings("MultiEditUtils.sublime-settings")
-		live_split_selection = settings.get("live_split_selection")
-
-		if live_split_selection:
-			onConfirm = None
-			onChange = self.splitSelection
-		else:
-			onConfirm = self.splitSelection
-			onChange = None
-
-		return (onConfirm, onChange)
+      inputView.run_command("select_all")
 
 
-	def restoreSelection(self):
+  def getHandlers(self):
 
-		selection = self.view.sel()
-		selection.clear()
-		for region in self.savedSelection:
-			selection.add(region)
+    settings = sublime.load_settings("MultiEditUtils.sublime-settings")
+    live_split_selection = settings.get("live_split_selection")
 
-		self.workaroundForRefreshBug(self.view, selection)
+    if live_split_selection:
+      onConfirm = None
+      onChange = self.splitSelection
+    else:
+      onConfirm = self.splitSelection
+      onChange = None
 
-
-	def splitSelection(self, separator):
-
-		view = self.view
-		newRegions = []
-
-		for region in self.savedSelection:
-			currentPosition = region.begin()
-			regionString = view.substr(region)
-
-			if separator:
-				subRegions = regionString.split(separator)
-			else:
-				# take each character separately
-				subRegions = list(regionString)
-
-			for subRegion in subRegions:
-				newRegion = sublime.Region(
-					currentPosition,
-					currentPosition + len(subRegion)
-				)
-				newRegions.append(newRegion)
-				currentPosition += len(subRegion) + len(separator)
-
-		selection = view.sel()
-		selection.clear()
-		for region in newRegions:
-			selection.add(region)
-
-		self.workaroundForRefreshBug(view, selection)
+    return (onConfirm, onChange)
 
 
-	def workaroundForRefreshBug(self, view, selection):
-		# work around sublime bug with caret position not refreshing
-		# see: https://github.com/code-orchestra/colt-sublime-plugin/commit/9e6ffbf573fc60b356665ff2ba9ced614c71120f
+  def restoreSelection(self):
 
-		bug = [s for s in selection]
-		view.add_regions("bug", bug, "bug", "dot", sublime.HIDDEN | sublime.PERSISTENT)
-		view.erase_regions("bug")
+    selection = self.view.sel()
+    selection.clear()
+    for region in self.savedSelection:
+      selection.add(region)
+
+    self.workaroundForRefreshBug(self.view, selection)
+
+
+  def splitSelection(self, separator):
+
+    view = self.view
+    newRegions = []
+
+    for region in self.savedSelection:
+      currentPosition = region.begin()
+      regionString = view.substr(region)
+
+      if separator:
+        subRegions = regionString.split(separator)
+      else:
+        # take each character separately
+        subRegions = list(regionString)
+
+      for subRegion in subRegions:
+        newRegion = sublime.Region(
+          currentPosition,
+          currentPosition + len(subRegion)
+        )
+        newRegions.append(newRegion)
+        currentPosition += len(subRegion) + len(separator)
+
+    selection = view.sel()
+    selection.clear()
+    for region in newRegions:
+      selection.add(region)
+
+    self.workaroundForRefreshBug(view, selection)
+
+
+  def workaroundForRefreshBug(self, view, selection):
+    # work around sublime bug with caret position not refreshing
+    # see: https://github.com/code-orchestra/colt-sublime-plugin/commit/9e6ffbf573fc60b356665ff2ba9ced614c71120f
+
+    bug = [s for s in selection]
+    view.add_regions("bug", bug, "bug", "dot", sublime.HIDDEN | sublime.PERSISTENT)
+    view.erase_regions("bug")
 
 
 
 class StripSelection(sublime_plugin.TextCommand):
 
-	def run(self, edit):
+  def run(self, edit):
 
-		newRegions = []
-		selection = self.view.sel()
+    newRegions = []
+    selection = self.view.sel()
 
-		for currentRegion in selection:
+    for currentRegion in selection:
 
-			text = self.view.substr(currentRegion)
+      text = self.view.substr(currentRegion)
 
-			lStrippedText = text.lstrip()
-			rStrippedText = lStrippedText.rstrip()
+      lStrippedText = text.lstrip()
+      rStrippedText = lStrippedText.rstrip()
 
-			lStrippedCount = len(text) - len(lStrippedText)
-			rStrippedCount = len(lStrippedText) - len(rStrippedText)
+      lStrippedCount = len(text) - len(lStrippedText)
+      rStrippedCount = len(lStrippedText) - len(rStrippedText)
 
-			a = currentRegion.begin() + lStrippedCount
-			b = currentRegion.end() - rStrippedCount
+      a = currentRegion.begin() + lStrippedCount
+      b = currentRegion.end() - rStrippedCount
 
-			if a == b:
-				# the region only contained whitespace
-				# use the old selection end to avoid jumping of cursor
-				a = b = currentRegion.b
+      if a == b:
+        # the region only contained whitespace
+        # use the old selection end to avoid jumping of cursor
+        a = b = currentRegion.b
 
-			newRegions.append(sublime.Region(a, b))
+      newRegions.append(sublime.Region(a, b))
 
 
-		selection.clear()
-		for region in newRegions:
-			selection.add(region)
+    selection.clear()
+    for region in newRegions:
+      selection.add(region)
 
 
 
 class RemoveEmptyRegions(sublime_plugin.TextCommand):
 
-	def run(self, edit):
+  def run(self, edit):
 
-		selection = self.view.sel()
-		regions = list(selection)
-		newRegions = list(filter(lambda r: not r.empty(), regions))
+    selection = self.view.sel()
+    regions = list(selection)
+    newRegions = list(filter(lambda r: not r.empty(), regions))
 
-		if len(newRegions) == 0:
-			sublime.status_message("There are only empty regions. Removing those would remove all regions. Aborting.")
-			return
+    if len(newRegions) == 0:
+      sublime.status_message("There are only empty regions. Removing those would remove all regions. Aborting.")
+      return
 
-		selection.clear()
-		for r in newRegions:
-			selection.add(r)
+    selection.clear()
+    for r in newRegions:
+      selection.add(r)
 
 
 
 class SelectionListener(sublime_plugin.EventListener):
 
-	def on_selection_modified(self, view):
+  def on_selection_modified(self, view):
 
-		helper = Helper.getOrConstructHelperForView(view)
-		lastSelections = helper.lastSelections
+    helper = Helper.getOrConstructHelperForView(view)
+    lastSelections = helper.lastSelections
 
-		if helper.ignoreSelectionCommand:
-			helper.ignoreSelectionCommand = False
-			return
+    if helper.ignoreSelectionCommand:
+      helper.ignoreSelectionCommand = False
+      return
 
-		currentSelection = view.sel()
+    currentSelection = view.sel()
 
-		if self.isComplexSelection(currentSelection):
+    if self.isComplexSelection(currentSelection):
 
-			currentRegions = list(currentSelection)
-			selectionWasExpanded = lastSelections and self.isSubsetOf(currentSelection, lastSelections[-1])
+      currentRegions = list(currentSelection)
+      selectionWasExpanded = lastSelections and self.isSubsetOf(currentSelection, lastSelections[-1])
 
-			if selectionWasExpanded:
-				# Override the last entry since the selection was expanded.
-				lastSelections[-1] = currentRegions
-			else:
-				lastSelections.append(currentRegions)
-
-
-	def isComplexSelection(self, selection):
-		# A "complex selection" is a selection which is not empty or has multiple regions.
-
-		regionCount = len(selection)
-
-		if not regionCount:
-			return False
-
-		firstRegionLength = len(selection[0])
-
-		return regionCount > 1 or firstRegionLength > 0
+      if selectionWasExpanded:
+        # Override the last entry since the selection was expanded.
+        lastSelections[-1] = currentRegions
+      else:
+        lastSelections.append(currentRegions)
 
 
-	def isSubsetOf(self, selectionA, selectionB):
-		# Check if selectionA is a subset of selectionB.
+  def isComplexSelection(self, selection):
+    # A "complex selection" is a selection which is not empty or has multiple regions.
 
-		return all(selectionA.contains(region) for region in selectionB)
+    regionCount = len(selection)
+
+    if not regionCount:
+      return False
+
+    firstRegionLength = len(selection[0])
+
+    return regionCount > 1 or firstRegionLength > 0
+
+
+  def isSubsetOf(self, selectionA, selectionB):
+    # Check if selectionA is a subset of selectionB.
+
+    return all(selectionA.contains(region) for region in selectionB)
 
 
 
 class TriggerSelectionModifiedCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit):
+  def run(self, edit):
 
-		SelectionListener().on_selection_modified(self.view)
+    SelectionListener().on_selection_modified(self.view)
 
 
 
 class Helper:
 
-	viewToHelperMap = {}
+  viewToHelperMap = {}
 
-	def __init__(self):
+  def __init__(self):
 
-		# The SelectionCommand should be ignored if it was triggered by AddLastSelectionCommand.
-		self.ignoreSelectionCommand = False
-		self.lastSelections = []
-
-
-	@staticmethod
-	def getOrConstructHelperForView(view):
-
-		mapping = Helper.viewToHelperMap
-		viewID = view.id()
-
-		if not viewID in mapping.keys():
-			mapping[viewID] = Helper()
-
-		helper = mapping[viewID]
-		return helper
+    # The SelectionCommand should be ignored if it was triggered by AddLastSelectionCommand.
+    self.ignoreSelectionCommand = False
+    self.lastSelections = []
 
 
-	@staticmethod
-	def hashSelection(selection):
+  @staticmethod
+  def getOrConstructHelperForView(view):
 
-		return str(list(selection))
+    mapping = Helper.viewToHelperMap
+    viewID = view.id()
+
+    if not viewID in mapping.keys():
+      mapping[viewID] = Helper()
+
+    helper = mapping[viewID]
+    return helper
+
+
+  @staticmethod
+  def hashSelection(selection):
+
+    return str(list(selection))
--- tests/testMultiEditUtils.py
+++ tests/testMultiEditUtils.py
@@ -5,199 +5,199 @@ version = sublime.version()
 
 class TestMultiEditUtils(TestCase):
 
-	def setUp(self):
+  def setUp(self):
 
-		self.view = sublime.active_window().new_file()
+    self.view = sublime.active_window().new_file()
 
 
-	def tearDown(self):
+  def tearDown(self):
 
-		if self.view:
-			self.view.set_scratch(True)
-			self.view.window().run_command("close_file")
+    if self.view:
+      self.view.set_scratch(True)
+      self.view.window().run_command("close_file")
 
 
-	def splitBy(self, separator, expectedAmount):
+  def splitBy(self, separator, expectedAmount):
 
-		testString = "this, is, a, test"
-		self.view.run_command("insert", {"characters": testString})
-		self.view.run_command("select_all")
-		self.view.run_command("split_selection", dict(separator = separator))
+    testString = "this, is, a, test"
+    self.view.run_command("insert", {"characters": testString})
+    self.view.run_command("select_all")
+    self.view.run_command("split_selection", dict(separator = separator))
 
-		selection = self.view.sel()
+    selection = self.view.sel()
 
-		self.assertEqual(len(selection), expectedAmount)
+    self.assertEqual(len(selection), expectedAmount)
 
 
-	def testSplitBySpace(self):
+  def testSplitBySpace(self):
 
-		self.splitBy(" ", 4)
+    self.splitBy(" ", 4)
 
 
-	def testSplitByCommaSpace(self):
+  def testSplitByCommaSpace(self):
 
-		self.splitBy(", ", 4)
+    self.splitBy(", ", 4)
 
 
-	def testSplitByCharacter(self):
+  def testSplitByCharacter(self):
 
-		self.splitBy("", 17)
+    self.splitBy("", 17)
 
 
-	def testToggleRegionEnds(self):
+  def testToggleRegionEnds(self):
 
-		testString = "this is a test"
-		self.view.run_command("insert", {"characters": testString})
+    testString = "this is a test"
+    self.view.run_command("insert", {"characters": testString})
 
-		regionTuple = [0, 14]
-		self.selectRegions([regionTuple])
+    regionTuple = [0, 14]
+    self.selectRegions([regionTuple])
 
-		selection = self.view.sel()
-		self.assertRegionEqual(selection[0], regionTuple)
+    selection = self.view.sel()
+    self.assertRegionEqual(selection[0], regionTuple)
 
-		self.view.run_command("normalize_region_ends")
+    self.view.run_command("normalize_region_ends")
 
-		self.assertRegionEqual(selection[0], regionTuple[::-1])
+    self.assertRegionEqual(selection[0], regionTuple[::-1])
 
 
-	def testToggleRegionEnds(self):
+  def testToggleRegionEnds(self):
 
-		testString = "test test"
-		self.view.run_command("insert", {"characters": testString})
+    testString = "test test"
+    self.view.run_command("insert", {"characters": testString})
 
-		regionTuples = [[0, 4], [9, 5]]
-		self.selectRegions(regionTuples)
+    regionTuples = [[0, 4], [9, 5]]
+    self.selectRegions(regionTuples)
 
-		selection = self.view.sel()
-		self.assertRegionEqual(selection[0], regionTuples[0])
-		self.assertRegionEqual(selection[1], regionTuples[1])
+    selection = self.view.sel()
+    self.assertRegionEqual(selection[0], regionTuples[0])
+    self.assertRegionEqual(selection[1], regionTuples[1])
 
-		self.view.run_command("normalize_region_ends")
+    self.view.run_command("normalize_region_ends")
 
-		self.assertRegionEqual(selection[0], regionTuples[0])
-		self.assertRegionEqual(selection[1], regionTuples[1][::-1])
+    self.assertRegionEqual(selection[0], regionTuples[0])
+    self.assertRegionEqual(selection[1], regionTuples[1][::-1])
 
 
-	def testJumpToLastRegion(self):
+  def testJumpToLastRegion(self):
 
-		testString = "test test test test"
-		self.view.run_command("insert", {"characters": testString})
+    testString = "test test test test"
+    self.view.run_command("insert", {"characters": testString})
 
-		self.selectRegions([[0, 4], [5, 9]])
+    self.selectRegions([[0, 4], [5, 9]])
 
-		selection = self.view.sel()
-		self.assertEqual(len(selection), 2)
+    selection = self.view.sel()
+    self.assertEqual(len(selection), 2)
 
-		self.view.run_command("jump_to_last_region")
+    self.view.run_command("jump_to_last_region")
 
-		self.assertEqual(len(selection), 1)
-		self.assertRegionEqual(selection[0], [9, 9])
+    self.assertEqual(len(selection), 1)
+    self.assertRegionEqual(selection[0], [9, 9])
 
 
-	def testAddLastSelection(self):
+  def testAddLastSelection(self):
 
-		testString = "this is a test"
-		self.view.run_command("insert", {"characters": testString})
+    testString = "this is a test"
+    self.view.run_command("insert", {"characters": testString})
 
-		regions = [[0, 4], [5, 9]]
-		self.selectRegions([regions[0]])
-		self.view.run_command("trigger_selection_modified")
-		self.selectRegions([regions[1]])
-		self.view.run_command("trigger_selection_modified")
+    regions = [[0, 4], [5, 9]]
+    self.selectRegions([regions[0]])
+    self.view.run_command("trigger_selection_modified")
+    self.selectRegions([regions[1]])
+    self.view.run_command("trigger_selection_modified")
 
-		self.view.run_command("add_last_selection")
+    self.view.run_command("add_last_selection")
 
-		selection = self.view.sel()
+    selection = self.view.sel()
 
 
-		self.assertEqual(len(selection), 2)
-		self.assertRegionEqual(selection[0], regions[0])
-		self.assertRegionEqual(selection[1], regions[1])
+    self.assertEqual(len(selection), 2)
+    self.assertRegionEqual(selection[0], regions[0])
+    self.assertRegionEqual(selection[1], regions[1])
 
 
-	def testRemoveEmptyRegions(self):
+  def testRemoveEmptyRegions(self):
 
-		testString = "a\nb\n\nc"
-		regions = [[0, 1], [2, 3], [5, 6]]
+    testString = "a\nb\n\nc"
+    regions = [[0, 1], [2, 3], [5, 6]]
 
-		self.view.run_command("insert", {"characters": testString})
-		self.view.run_command("select_all")
-		self.view.run_command("split_selection_into_lines")
-		self.view.run_command("remove_empty_regions")
+    self.view.run_command("insert", {"characters": testString})
+    self.view.run_command("select_all")
+    self.view.run_command("split_selection_into_lines")
+    self.view.run_command("remove_empty_regions")
 
-		selection = self.view.sel()
+    selection = self.view.sel()
 
-		self.assertEqual(len(selection), 3)
+    self.assertEqual(len(selection), 3)
 
-		for actual, expected in zip(selection, regions):
-			self.assertRegionEqual(actual, expected)
+    for actual, expected in zip(selection, regions):
+      self.assertRegionEqual(actual, expected)
 
 
-	def testStripSelection(self):
+  def testStripSelection(self):
 
-		testString = "  too much whitespace here  "
+    testString = "  too much whitespace here  "
 
-		self.view.run_command("insert", {"characters": testString})
-		self.view.run_command("select_all")
-		self.view.run_command("strip_selection")
+    self.view.run_command("insert", {"characters": testString})
+    self.view.run_command("select_all")
+    self.view.run_command("strip_selection")
 
-		selection = self.view.sel()
+    selection = self.view.sel()
 
-		self.assertEqual(len(selection), 1)
-		self.assertRegionEqual(selection[0], [2, 26])
+    self.assertEqual(len(selection), 1)
+    self.assertRegionEqual(selection[0], [2, 26])
 
 
-	def testStripSelectionWithPureWhitespace(self):
+  def testStripSelectionWithPureWhitespace(self):
 
-		testString = "    "
+    testString = "    "
 
-		self.view.run_command("insert", {"characters": testString})
-		selection = self.view.sel()
+    self.view.run_command("insert", {"characters": testString})
+    selection = self.view.sel()
 
-		# cursor should stay at the end of the line
-		self.view.run_command("select_all")
-		self.view.run_command("strip_selection")
+    # cursor should stay at the end of the line
+    self.view.run_command("select_all")
+    self.view.run_command("strip_selection")
 
-		self.assertEqual(len(selection), 1)
-		self.assertRegionEqual(selection[0], [4, 4])
+    self.assertEqual(len(selection), 1)
+    self.assertRegionEqual(selection[0], [4, 4])
 
-		# cursor should be at the beginning of the line
-		self.view.run_command("select_all")
-		self.view.run_command("normalize_region_ends")
-		self.view.run_command("strip_selection")
+    # cursor should be at the beginning of the line
+    self.view.run_command("select_all")
+    self.view.run_command("normalize_region_ends")
+    self.view.run_command("strip_selection")
 
-		self.assertEqual(len(selection), 1)
-		self.assertRegionEqual(selection[0], [0, 0])
+    self.assertEqual(len(selection), 1)
+    self.assertRegionEqual(selection[0], [0, 0])
 
 
-	def testMultiFindAll(self):
+  def testMultiFindAll(self):
 
-		testString = "abc def - abc - def - def"
+    testString = "abc def - abc - def - def"
 
-		self.view.run_command("insert", {"characters": testString})
-		selection = self.view.sel()
+    self.view.run_command("insert", {"characters": testString})
+    selection = self.view.sel()
 
-		# select the first occurrences of abc and def
-		selection.add(sublime.Region(0, 3))
-		selection.add(sublime.Region(4, 7))
+    # select the first occurrences of abc and def
+    selection.add(sublime.Region(0, 3))
+    selection.add(sublime.Region(4, 7))
 
-		self.view.run_command("multi_find_all")
+    self.view.run_command("multi_find_all")
 
-		self.assertEqual(len(selection), 5)
-		expectedRegions = [[0, 3], [4, 7], [10, 13], [16, 19], [22, 25]]
+    self.assertEqual(len(selection), 5)
+    expectedRegions = [[0, 3], [4, 7], [10, 13], [16, 19], [22, 25]]
 
-		for index, region in enumerate(expectedRegions):
-			self.assertRegionEqual(selection[index], region)
+    for index, region in enumerate(expectedRegions):
+      self.assertRegionEqual(selection[index], region)
 
 
-	def assertRegionEqual(self, a, b):
+  def assertRegionEqual(self, a, b):
 
-		self.assertEqual(a.a, b[0])
-		self.assertEqual(a.b, b[1])
+    self.assertEqual(a.a, b[0])
+    self.assertEqual(a.b, b[1])
 
 
-	def selectRegions(self, regions):
+  def selectRegions(self, regions):
 
-		self.view.sel().clear()
-		for regionTuple in regions:
-			self.view.sel().add(sublime.Region(regionTuple[0], regionTuple[1]))
+    self.view.sel().clear()
+    for regionTuple in regions:
+      self.view.sel().add(sublime.Region(regionTuple[0], regionTuple[1]))
